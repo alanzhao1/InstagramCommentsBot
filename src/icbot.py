78e4aa743df69e142154c45eadb6ae4b3146de22
Alan Zhao <ayz2102@columbia.edu>
2016-02-21 17:39:55 -0500

    Switched to OAuth2 in main packages, continued work on main code

--- src/icbot.py
+++ src/icbot.py
@@ -5,23 +5,48 @@ import praw
 import os
 import sqlite3
 import logging
+import json
 
+from prawoauth2 import PrawOAuth2Mini
+from tokens import app_key, app_secret, access_token, refresh_token
+from settings import user_agent, scopes
 
-USER_AGENT = "InstagramCommentsBot v. BETA 1.0 by /u/whereisspacebar"
+# TODO: logging setup
+#loglevel = logging.DEBUG if DEBUG else logging.INFO
+#logging.basicConfig(level=loglevel,
+#log = logging.getLogger('icbot')
 
-r = praw.Reddit(USER_AGENT)
+# reddit api setup
+reddit_client = praw.Reddit(user_agent)
+oauth_helper = PrawOAuth2Mini(reddit_client, app_key = app_key,
+        app_secret == app_secret, access_token = access_token,
+        scopes = scopes, refresh_token = refresh_token)
 
-
-class icbot:
+class ICBot:
     
-    def __init__(self, username, password, limit=25):
-        self.username = username
-        self.password = password
+    def __init__(self,limit=25):
         self.limit = limit
+   
+    def run():
+        pass
 
+    def _setup(self):
+        
+        self.issetup = True
+        
 
 if __name__ == "__main__":
-    username = os.environ.get(REDDIT_USERNAME)
-    password = os.environ.get(REDDIT_PASSWORD)
     
-    myBot = icBot(username, password)
+    icbot = ICBot()
+
+    try:
+        while True:
+            try:
+                icbot.run()
+            except RECOVERABLE_EXEC as e:
+                pass
+                # TODO: handle errors via logger
+    except KeyboardInterrupt:
+        pass
+
+
